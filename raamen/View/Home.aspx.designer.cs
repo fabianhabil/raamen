37ab65bc11901b769a340f24444356b9453e0100
fabianhabil <fabianhabilramdhan@gmail.com>
2023-05-10 23:24:37 +0700

    remote

--- raamen/View/Home.aspx.designer.cs
+++ raamen/View/Home.aspx.designer.cs
@@ -11,14 +11,5 @@ namespace raamen.View {
 
 
     public partial class Home {
-
-        /// <summary>
-        /// Button1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button1;
     }
 }
