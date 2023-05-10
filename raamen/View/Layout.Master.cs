c67b2fbf006597ae2875fa9f2ae7c6d17c8cb11f
fabianhabil <fabianhabilramdhan@gmail.com>
2023-05-11 01:47:38 +0700

    onprog: navbar layout

--- raamen/View/Layout.Master.cs
+++ raamen/View/Layout.Master.cs
@@ -1,14 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace raamen.View {
     public partial class Layout : System.Web.UI.MasterPage {
         protected void Page_Load(object sender, EventArgs e) {
-
+            username.Visible = true;
         }
     }
 }
--- raamen/View/Layout.Master.designer.cs
+++ raamen/View/Layout.Master.designer.cs
@@ -31,6 +31,24 @@ namespace raamen.View {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
+        /// username control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlAnchor username;
+
+        /// <summary>
+        /// user control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label user;
+
+        /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
