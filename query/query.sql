6ac6c6962aa7ff28951fd8168970ae402e00f5a8
fabianhabil <fabianhabilramdhan@gmail.com>
2023-06-08 20:00:09 +0700

    database version still error

--- query/query.sql
+++ query/query.sql
@@ -38,3 +38,9 @@ CREATE TABLE [Detail] (
 	Quantity INT NOT NULL,
     Constraint DetailId PRIMARY KEY (HeaderId, RamenId)
 )
+
+INSERT INTO [Role] (Name) VALUES ('Customer');
+INSERT INTO [Role] (Name) VALUES ('Staff');
+INSERT INTO [Role] (Name) VALUES ('Admin');
+INSERT INTO [User] VALUES (2, 'staff', 'staff@gmail.com', 'Male', '123');
+INSERT INTO [User] VALUES (3, 'admin', 'admin@gmail.com', 'Male', '123');
